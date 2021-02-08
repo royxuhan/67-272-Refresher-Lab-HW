f932ffbd5aabdd5729a7e471bd3cb2800c0f96de
Roy <royxuhan@gmail.com>
2021-02-07 17:09:12 -0800

    Name method

--- app/models/child.rb
+++ app/models/child.rb
@@ -8,4 +8,8 @@ class Child < ApplicationRecord
   scope :active, -> { where(active: true) }
 
   validates_presence_of :first_name, :last_name
+
+  def name
+    first_name + ' ' + last_name
+  end
 end
