4b245759ef13e73d043f1f07d473619336a5e0b6
Roy <royxuhan@gmail.com>
2021-02-07 17:08:22 -0800

    Child model

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 class Child < ApplicationRecord
-    
+  has_many :chores
+  has_many :tasks, through: :chores
+
+  scope :alphabetical, -> { order('last_name, first_name') }
+  scope :active, -> { where(active: true) }
+
+  validates_presence_of :first_name, :last_name
 end
