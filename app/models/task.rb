0acfed2fbbcf21f63b69b73dd2c3110b378e8a2c
Roy <royxuhan@gmail.com>
2021-02-07 17:12:29 -0800

    Task model and test files

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 class Task < ApplicationRecord
+  has_many :chores
+  has_many :children, through: :chores
+
+  scope :alphabetical, -> { order('name') }
+  scope :active, -> { where(active: true) }
+
+  validates_presence_of :name
+  validates_numericality_of :points, only_integer: true, greater_than: 0
 end
